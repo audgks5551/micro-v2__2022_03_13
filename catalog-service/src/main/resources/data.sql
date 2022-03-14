652652d4ef1c7f18625cda69fc40006553eef564
audgks5551 <audgks5551@daum.net>
2022-03-14 21:21:01 +0900

    order, user, catalog 구현

---
+++ catalog-service/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into catalog(product_id, product_name, stock, unit_price)
+    values('CATALOG-001', 'A', 100, 1500);
+
+insert into catalog(product_id, product_name, stock, unit_price)
+    values('CATALOG-002', 'B', 110, 1600);
+
+insert into catalog(product_id, product_name, stock, unit_price)
+    values('CATALOG-003', 'C', 120, 1700);
